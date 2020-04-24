1c7a621450bb5774e1d6fc696b6e8ed1bfb719c8
primeairtime <52601484+primeairtime@users.noreply.github.com>
2020-04-24 14:44:37 +0100

    Update config.php

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ return array(
     'display_error' => false, // TRUE or FALSE
     'use_token' => false, // TRUE or FALSE
     'token' => '############', //if use_token is set to true
-    'username' => 'seuntech2k@yahoo.com', //if use_token is set to false
-    'password' => '1111', //if use_token is set to false
+    'username' => '######@yahoo.com', //if use_token is set to false
+    'password' => '###', //if use_token is set to false
     'enviroment' => 'test', //live
     )
 ?>
